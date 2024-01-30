df5d9ff83532f83416df8a9bd115b467ff8f05d2
amitvishwa19 <amitvishwa19@gmail.com>
2024-01-30 11:56:41 +0530

    socket.io

--- index.js
+++ index.js
@@ -13,7 +13,27 @@ const io = new Server(server);
 const port = process.env.PORT || 9001;
 
 app.get('/dev/chat', (req, res) => {
-    res.send('<h1>Hello world</h1>');
+    if (!res.socket.server.io) {
+        console.log('*First use, starting Socket.IO');
+        const io = new Server(res.socket.server);
+
+        // Listen for connection events
+        io.on('connection', (socket) => {
+            console.log(`Socket ${socket.id} connected.`);
+
+            // Listen for incoming messages and broadcast to all clients
+            socket.on('message', (message) => {
+                io.emit('message', message);
+            });
+
+            // Clean up the socket on disconnect
+            socket.on('disconnect', () => {
+                console.log(`Socket ${socket.id} disconnected.`);
+            });
+        });
+        res.socket.server.io = io;
+    }
+    res.end();
 });
 
 io.on('connection', (socket) => {
