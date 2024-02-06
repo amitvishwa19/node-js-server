8833f7f45f850c33fced6bf71b8bbdaa3a219ccf
amitvishwa19 <amitvishwa19@gmail.com>
2024-02-06 21:51:27 +0530

    cors edit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const router = express.Router()
 
 const app = express();
 app.use(express.json())
-app.use(cors())
+app.use(cors({origin: ['http://localhost:3000', 'http://127.0.0.1:3000']}))
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
