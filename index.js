a938e40c4fea00deabda1872a96d849afa058c52
amitvishwa19 <amitvishwa19@gmail.com>
2024-02-06 19:40:02 +0530

    chat & video-1

--- index.js
+++ index.js
@@ -28,81 +28,75 @@ const socket_port = process.env.SOCKET_PORT || 9000;
 const mongo_uri = process.env.MONGODB_URI;
 
 
-app.use('/api/v1/auth', auth);
+//app.use('/api/v1/auth', auth);
 //app.use('/api/v1/auth/register', register);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.get('/api/v1/chat', (req, res) => {
-    if (!res.socket.server.io) {
-        console.log('*First use, starting Socket.IO');
-        const io = new Server(res.socket.server);
-
-        // Listen for connection events
-        io.on('connection', (socket) => {
-            console.log(`Socket ${socket.id} connected.`);
-
-            // Listen for incoming messages and broadcast to all clients
-            socket.on('myevent', (data) => {
-                socket.emit('responseEvent', 'Hwllo client from server');
-            });
-
-            // Clean up the socket on disconnect
-            socket.on('disconnect', () => {
-                console.log(`Socket ${socket.id} disconnected.`);
-            });
-        });
-        res.socket.server.io = io;
-    }
-    res.end();
-    res.send('Welcome to chat')
-});
 
 
 let onlineUsers = []
 
 io.on('connection', (socket) => {
-    console.log('socket-id', socket.id)
+    console.log('client connected io-socket', socket.id)
 
-    socket.on('addNewUser', (uid) => {
-
-        if (onlineUsers.findIndex(item => item.uid === uid) === -1) {
+    socket.on('new-user', (user) => {
+        console.log('Adding onlineuser')
+        if (onlineUsers.findIndex(item => item.id === user?._id) === -1) {
             onlineUsers.push({
-                uid,
+                uid: user?._id,
+                email: user?.email,
+                username: user?.username,
                 socketId: socket.id
             })
         }
 
         io.emit('getOnlineUsers', onlineUsers)
+        io.emit('online-users', onlineUsers)
 
         console.log('Online users from socket.io', onlineUsers)
 
     })
 
-    socket.on('myevent', (data) => {
-        console.log('Client', data)
-        socket.emit('responseEvent', 'Hwllo client from server');
-    });
 
-
-    //Sending new message
-    socket.on('newMessage',(data)=>{
-        console.log('new msg received',data)
+    socket.on('new-message', (data) => {
+        console.log('new msg received on server')
         //we get members from chat id and send message to all of them
         //io.to(socketId).emit('getNewMessage',(data)=>{}) 
-        io.emit('newMessage',data)
+        io.emit('new-message', data)
     })
+
+
+    socket.on('new-chat', (data) => {
+        console.log('new chat received on server')
+        io.emit('new-chat', data)
+    })
+
+
+    socket.on("room:join", (data) => {
+        const { email, room } = data;
+        emailToSocketIdMap.set(email, socket.id);
+        socketidToEmailMap.set(socket.id, email);
+        io.to(room).emit("user:joined", { email, id: socket.id });
+        socket.join(room);
+        io.to(socket.id).emit("room:join", data);
+    });
+
+    socket.on("user:call", ({ to, offer }) => {
+        io.to(to).emit("incomming:call", { from: socket.id, offer });
+    });
+
+    socket.on("call:accepted", ({ to, ans }) => {
+        io.to(to).emit("call:accepted", { from: socket.id, ans });
+    });
+
+    socket.on("peer:nego:needed", ({ to, offer }) => {
+        console.log("peer:nego:needed", offer);
+        io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+    });
+
+    socket.on("peer:nego:done", ({ to, ans }) => {
+        console.log("peer:nego:done", ans);
+        io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+    });
+
 
     socket.on('disconnect', () => {
         console.log('user disconnected', socket.id)
